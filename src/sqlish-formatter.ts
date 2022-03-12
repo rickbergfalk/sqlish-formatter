2729590bdf42a2cfd5264cf814287c41e33839c5
Rick Bergfalk <rick.bergfalk@gmail.com>
2022-03-11 23:51:47 -0600

    Add an enum for types

--- src/sqlish-formatter.ts
+++ src/sqlish-formatter.ts
@@ -1,10 +1,32 @@
 import { lexer } from "./keywords";
 
+enum Types {
+  comma = "comma",
+  comment = "comment",
+  identifier = "identifier",
+  keyword = "keyword",
+  lparen = "lparen",
+  number = "number",
+  operator = "operator",
+  period = "period",
+  quotedIdentifier = "quotedIdentifer",
+  rparen = "rparen",
+  string = "string",
+  terminator = "terminator",
+  whitespace = "whitespace",
+}
+
 export function format(sql: string) {
   lexer.reset(sql);
 
-  let tokens = Array.from(lexer);
+  let tokens: moo.Token[] = Array.from(lexer);
   console.log(tokens);
+
+  const first = tokens[0];
+
+  if (first.type === Types.keyword) {
+    // and then do something here I guess
+  }
 
   return `hello world`;
 }
